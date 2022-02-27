cb81cfad1cb5cc775976b1bf443917f5165a8231
lukaloma3 <luka3lomadze@gmail.com>
2022-02-27 08:51:02 +0400

    angular1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang1';
+title = 'ang1';
+v = true
+old = 'ძველი მნიშვნელობა'
+new1= 'ახალი მნიშვნელობა'
+videosrc = {
+vid: '../assets/video/mov_bbb.mp4'
+
+}
+
+
+change1(){
+this.v = !this.v
+  }
 }
